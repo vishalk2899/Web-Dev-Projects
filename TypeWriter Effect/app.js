d3670ab97bd80a244d0a0ecb2635d4456246e6fc
Vishal Kshatriya <kshatriya789789@gmail.com>
2020-08-01 14:50:52 +0530

    Add files via upload

---
+++ TypeWriter Effect/app.js
@@ -0,0 +1,21 @@
+const texts = ['Dont You Give Up nah nah nah','I wont Give Up nah nah nah','Let me Love You','Let me Love You'];
+
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type(){
+  // if(count === texts.length){
+  //   count = 0;
+  // }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector('.typing').textContent = letter;
+  if(letter.length === currentText.length){
+    count++;
+    index =0;
+  }
+  setTimeout(type, 150);
+}());
