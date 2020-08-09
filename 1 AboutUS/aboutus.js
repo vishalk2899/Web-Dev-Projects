fad1597585e28bcfa607e5559fd86c9804782569
Vishal Kshatriya <kshatriya789789@gmail.com>
2020-08-09 12:04:00 +0530

    Add files via upload

---
+++ 1 AboutUS/aboutus.js
@@ -0,0 +1,12 @@
+// $("html").easeScroll({
+//   frameRate: 20,
+//   animationTime: 100,
+//   stepSize: 1,
+//   pulseAlgorithm: !0,
+//   pulseScale: 8,
+//   pulseNormalize: 1,
+//   accelerationDelta: 20,
+//   accelerationMax: 1,
+//   keyboardSupport: !0,
+//   arrowScroll: 50
+// });
